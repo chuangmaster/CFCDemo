6d84c146fbb866c0c8373668baf97eaab182c034
chuangmaster <chuang.master@gmail.com>
2019-06-18 01:40:50 +0800

    加入DA內容(Data Annotation)

--- CFCDemo/Product.cs
+++ CFCDemo/Product.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CFCDemo
 {
+    [Table("KProduct")]
     public class Product
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "ProductName最大長度為50"), MinLength(2)]
+        [Column("ProductName")]
+        [Index("ProductNameIndex", 1, IsUnique = true)]
         public string Name { get; set; }
+        [Column("ListPirce", TypeName = "Money")]
         public int? UnitPrice { get; set; }
-        public string Category { get; set; }
+        [Index("CategoryIndex", 2)]
+        public int Category { get; set; }
+        [NotMapped]
+        public decimal SPrice { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime Created { get; set; }
     }
 }
